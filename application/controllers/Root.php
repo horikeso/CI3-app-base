3742123329dfda8197890aadcee95c2575f92185
horikeso <horiguchi.android@gmail.com>
2017-11-28 13:07:25 +0900

    CIのファイルアップロードクラスに変更

--- application/controllers/Root.php
+++ application/controllers/Root.php
@@ -19,55 +19,18 @@ class Root extends CI_Controller {
 
         try
         {
-            if ( ! empty($_FILES))
+            // アップロード設定
+            $config['upload_path'] = APPPATH . '../public/uploads/';
+            $config['allowed_types'] = 'jpg|png';
+            $config['file_name'] = 'images';// 拡張子を省略すると元々の拡張子で保存してくれます。
+            $config['overwrite'] = true;
+
+            $this->load->library('upload', $config);
+
+            // アップロード
+            if ( ! $this->upload->do_upload('file'))
             {
-                // ファイルは複数ではない
-                if (count($_FILES['file']['name']) > 1)
-                {
-                    // error処理
-                    throw new RuntimeException('ファイルが複数設定されています。');
-                }
-
-                // エラーがない
-                switch ($_FILES['file']['error'])
-                {
-                    case UPLOAD_ERR_OK:
-                        break;
-                    case UPLOAD_ERR_INI_SIZE:
-                    case UPLOAD_ERR_FORM_SIZE:
-                        throw new RuntimeException('ファイルのサイズが大きるためアップロードされませんでした。');
-                    case UPLOAD_ERR_PARTIAL:
-                        throw new RuntimeException('ファイルは完全にアップロードされませんでした。');
-                    case UPLOAD_ERR_NO_FILE:
-                        throw new RuntimeException('ファイルはアップロードされませんでした。');
-                    case UPLOAD_ERR_NO_TMP_DIR:
-                        throw new RuntimeException('テンポラリフォルダがありません。');
-                    case UPLOAD_ERR_CANT_WRITE:
-                        throw new RuntimeException('ディスクへの書き込みに失敗しました。');
-                    case UPLOAD_ERR_EXTENSION:
-                        throw new RuntimeException('異常が発生したためアップロードに失敗しました。');
-                }
-
-                // MIMEチェック
-                $mime_type = mime_content_type($_FILES['file']['tmp_name']);
-                $match_result = preg_match('/^image\/(jpeg|png)$/', $mime_type);
-                if ($match_result === 0 || $match_result === false)
-                {
-                    throw new RuntimeException('許可されていないファイル形式です。');
-                }
-
-                $uploaddir = APPPATH . '../public/uploads/';
-                $uploadfile = $uploaddir . basename($_FILES['file']['name']);
-
-                if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile) === false) {
-                    throw new RuntimeException('ファイルのアップロードに失敗しました。');
-                }
-
-                chmod($uploadfile, 0744);
-            }
-            else
-            {
-                throw new RuntimeException('ファイルが設定されていません');
+                throw new RuntimeException($this->upload->display_errors());
             }
         }
         catch (RuntimeException $exception)
